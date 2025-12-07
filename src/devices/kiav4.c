7f5638fdfb8970c837e26f2e038e8adef3d71c67
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2025-12-07 11:46:28 +0100

    Add decoding functionality with enduser provided keys

--- src/devices/kiav4.c
+++ src/devices/kiav4.c
@@ -6,10 +6,12 @@
     https://discord.gg/UYUYGUk8
 
     v3/4 uses keeloq keys which I am not going to distribute
-    so the keeloq decoding function and keys have been stripped
+    decoding will require the user to put in keys
 */
 
 #include "decoder.h"
+                            //V4     V5
+uint64_t kia_3_4_mf_keys[] = {0x00, 0x00};
 
 static int kia_v4_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {   
@@ -58,17 +60,50 @@ static int kia_v4_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     int serial         = (reverse8(b[7] & 0xf0) << 24) | (reverse8(b[6]) << 16) | (reverse8(b[5]) << 8) | (reverse8(b[4]));
     int btn            = ((reverse8(b[7]) & 0xF0) >> 4);
 
-    char key_str[18];
-    snprintf(key_str, sizeof(key_str), "%08lX", key);
-    char yek_str[18];
-    snprintf(yek_str, sizeof(yek_str), "%08lX", yek);
-    char encrypted_str[9];
-    snprintf(encrypted_str, sizeof(encrypted_str), "%08X", encrypted);
-    char serial_str[9];
-    snprintf(serial_str, sizeof(serial_str), "%07X", serial);
-    char model_str[] = "Kia 3/4";
+    if (kia_3_4_mf_keys[0] == 0x00) {
+        char key_str[18];
+        snprintf(key_str, sizeof(key_str), "%08lX", key);
+        char yek_str[18];
+        snprintf(yek_str, sizeof(yek_str), "%08lX", yek);
+        char encrypted_str[9];
+        snprintf(encrypted_str, sizeof(encrypted_str), "%08X", encrypted);
+        char serial_str[9];
+        snprintf(serial_str, sizeof(serial_str), "%07X", serial);
+        char model_str[] = "Kia 3/4";
+    }
+    else {
+        char key_str[18];
+        snprintf(key_str, sizeof(key_str), "%08lX", key);
+        char yek_str[18];
+        snprintf(yek_str, sizeof(yek_str), "%08lX", yek);
+        char encrypted_str[9];
+        snprintf(encrypted_str, sizeof(encrypted_str), "%08X", encrypted);
+        char serial_str[9];
+        snprintf(serial_str, sizeof(serial_str), "%07X", serial);
+        char model_str[] = "Unknown Kia 3/4";
+        char decrypted_str[12];
+        snprintf(decrypted_str, sizeof(decrypted_str), "%s", "Unknown");
+        char mfkey_str[18];
+        snprintf(mfkey_str, sizeof(mfkey_str), "%s", "Unknown");
+
+        for (size_t j = 0; j < 2; j++){ 
+            uint32_t block = keeloq_common_decrypt(encrypted, kia_3_4_mf_keys[j]);
+            if (((uint8_t)(btn) == (uint8_t)(block >> 28)) && ((uint8_t)(serial & 0x00000FF)) == ((uint8_t)((block & 0x00FF0000) >> 16))) {
+                snprintf(decrypted_str, sizeof(decrypted_str), "%08X", block);
+                snprintf(mfkey_str, sizeof(mfkey_str), "%08lX", kia_3_4_mf_keys[j]);
+            
+                if (kia_3_4_mf_keys[j] == kia_3_4_mf_keys[0]) {
+                    snprintf(model_str, sizeof(model_str), "%s", "Kia V4");
+                }
+                else {
+                    snprintf(model_str, sizeof(model_str), "%s", "Kia V3");
+                }
+                break;
+            }
+        }
+    }
     
-    /* clang-format off */
+     /* clang-format off */
     data_t *data = data_make(
             "model",            "",             DATA_STRING,    model_str,
             "key",              "",             DATA_STRING,    key_str,
@@ -76,6 +111,8 @@ static int kia_v4_decode(r_device *decoder, bitbuffer_t *bitbuffer)
             "button",           "",             DATA_INT,       btn,
             "yek",              "",             DATA_STRING,    yek_str,
             "serial",           "",             DATA_STRING,    serial_str,
+            "decrypted",        "",             DATA_STRING,    decrypted_str,
+            "mfkey",            "",             DATA_STRING,    mfkey_str,
             NULL);
     /* clang-format on */
     
@@ -91,6 +128,8 @@ static char const *const output_fields[] = {
         "button",
         "yek",  
         "serial",      
+        "decrypted",
+        "mfkey",
         NULL,
 };
 
